212c1dd5fee7521f297ca16f59b9f343b226b744
Edik Kurbatov <iamedikkurbat0v@gmail.com>
2026-02-03 09:57:47 +0300

    Fixed errors found after running the command "lint"

--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,7 @@
-import readlineSync from 'readline-sync';
+import readlineSync from 'readline-sync'
 
 export default () => {
   console.log('Welcome to the Brain Games!')
   const nameUser = readlineSync.question('May I have your name? ')
   console.log(`Hello, ${nameUser}!`)
 }
-
-
-
