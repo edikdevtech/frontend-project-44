b6faa7ca60af171b4628892035204e76f89150ff
Edik Kurbatov <iamedikkurbat0v@gmail.com>
2026-02-05 14:02:05 +0300

    add code games of "brain-calc" and "brain-even"

---
+++ src/calc.js
@@ -0,0 +1,42 @@
+import readlineSync from 'readline-sync'
+import { nameUser } from '../src/cli.js'
+import { numberOfQuestionsInGame } from '../src/index.js'
+
+export default () => {
+  console.log('What is the result of the expression?')
+
+  for (let i = 1; i <= numberOfQuestionsInGame; i++) {
+    let randomNumber1 = Math.round(Math.random() * 10)
+    let randomNumber2 = Math.round(Math.random() * 10)
+    const calculationOperators = ['-', '+', '*']
+    const randomOperator = calculationOperators[Math.floor(Math.random() * calculationOperators.length)]
+
+    console.log(`Question: ${randomNumber1} ${randomOperator} ${randomNumber2}`)
+
+    let resultQuestion
+
+    if (randomOperator === '+') {
+      resultQuestion = randomNumber1 + randomNumber2
+    }
+    else if (randomOperator === '-') {
+      resultQuestion = randomNumber1 - randomNumber2
+    }
+    else if (randomOperator === '*') {
+      resultQuestion = randomNumber1 * randomNumber2
+    }
+
+    const answer = parseInt(readlineSync.question('Your answer: '))
+
+    if (answer === resultQuestion) {
+      console.log('Correct!')
+    }
+    else {
+      console.log(`"${answer}" is wrong answer ;(. Correct answer was "${resultQuestion}".\nLet's try again, ${nameUser}`)
+      break
+    }
+
+    if (i === numberOfQuestionsInGame) {
+      console.log(`Congratulations, ${nameUser}!`)
+    }
+  }
+}
---
+++ src/even.js
@@ -0,0 +1,39 @@
+import readlineSync from 'readline-sync'
+import { nameUser } from '../src/cli.js'
+import { numberOfQuestionsInGame } from '../src/index.js'
+
+export default () => {
+  console.log('Answer "yes" if the number is even, otherwise answer "no".')
+
+  for (let i = 1; i <= numberOfQuestionsInGame; i++) {
+    let randomNumber = Math.round(Math.random() * 100) + 1
+    console.log(`Question: ${randomNumber}`)
+    const answer = readlineSync.question('Your answer: ', {
+      limit: ['yes', 'no'],
+      trueValue: ['yes'],
+      falseValue: ['no'],
+    })
+
+    if (randomNumber % 2 === 0) {
+      if (answer === true) {
+        console.log('Correct!')
+      }
+      else if (answer === false) {
+        console.log(`'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${nameUser}`)
+        break
+      }
+    }
+    else {
+      if (answer === false) {
+        console.log('Correct!')
+      }
+      else if (answer === true) {
+        console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${nameUser}!`)
+        break
+      }
+    }
+    if (i === numberOfQuestionsInGame) {
+      console.log(`Congratulations, ${nameUser}!`)
+    }
+  }
+}
