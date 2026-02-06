24759169ada6193ddd3eddd388d4832a48449f71
Edik Kurbatov <iamedikkurbat0v@gmail.com>
2026-02-06 14:50:18 +0300

    add code game "gcd"

---
+++ src/gcd.js
@@ -0,0 +1,48 @@
+import readlineSync from 'readline-sync'
+import { nameUser } from '../src/cli.js'
+import { numberOfQuestionsInGame } from '../src/index.js'
+
+export default () => {
+  console.log('Find the greatest common divisor of given numbers.')
+
+  for (let i = 1; i <= numberOfQuestionsInGame; i++) {
+    let randomNumber1 = Math.round(Math.random() * 100) + 1
+    let randomNumber2 = Math.round(Math.random() * 100) + 1
+
+    console.log(`Question ${randomNumber1} ${randomNumber2}`)
+    const answer = parseInt(readlineSync.question('Your answer: '))
+    let resultArr = []
+
+    for (let i = 1; i <= randomNumber1; i++) {
+      if (randomNumber1 % i === 0) {
+        resultArr.push(i)
+      }
+    }
+
+    for (let i = 1; i <= randomNumber2; i++) {
+      if (randomNumber2 % i === 0) {
+        resultArr.push(i)
+      }
+    }
+
+    const dublicates = resultArr.filter(function firstEntry(item, index, resultArr) {
+      if (resultArr.indexOf(item) !== index) {
+        return item
+      }
+    })
+
+    let maxNumberArr = Math.max(...dublicates)
+
+    if (answer === maxNumberArr) {
+      console.log('Correct!')
+    }
+    else {
+      console.log(`"${answer}" is wrong answer ;(. Correct answer was "${maxNumberArr}".\nLet's try again, ${nameUser}`)
+      break
+    }
+
+    if (i === numberOfQuestionsInGame) {
+      console.log(`Congratulations, ${nameUser}!`)
+    }
+  }
+}
