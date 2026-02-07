b992ce19351563c981e5f54423ddcf1fb72e340c
Edik Kurbatov <iamedikkurbat0v@gmail.com>
2026-02-07 21:19:34 +0300

    add a new export for the variable "nameUser"

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync'
 
-let nameUser = ''
+export let nameUser = ''
 export default function hiUser() {
   console.log('Welcome to the Brain Games!')
   nameUser = readlineSync.question('May I have your name? ')
