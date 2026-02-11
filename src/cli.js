a8a624803aa79dc4d1a948800af8c6e016c1a14c
Edik Kurbatov <iamedikkurbat0v@gmail.com>
2026-02-11 09:08:04 +0300

    replace export

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync'
 
 let nameUser = ''
+
 export default function hiUser() {
   console.log('Welcome to the Brain Games!')
   nameUser = readlineSync.question('May I have your name? ')
