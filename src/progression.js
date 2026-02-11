aaba9458b6b7252f77dc76ab98e2b7d360576469
Edik Kurbatov <iamedikkurbat0v@gmail.com>
2026-02-11 07:28:02 +0300

    imported a new function for creating an arithmetic progression and finding a missing element in it and improved the code

--- src/progression.js
+++ src/progression.js
@@ -1,38 +1,25 @@
 import readlineSync from 'readline-sync'
 import { nameUser } from '../src/cli.js'
-import { numberOfQuestionsInGame } from '../src/index.js'
+import { numberOfQuestionsInGame, progressionCode } from '../src/index.js'
 
 export default function progression() {
   console.log('What number is missing in the progression?')
 
   for (let i = 1; i <= numberOfQuestionsInGame; i++) {
-    const lengthProgression = Math.round(Math.random() * 10) + 10
-    const missingElementProgression = Math.round(Math.random() * 10) + 3
-    const startElementProgression = Math.round(Math.random() * 10) + 1
-    const stepProgression = Math.round(Math.random() * 10) + 1
-    let elementProgressionUser
-    let elementProgressionAuthor
-    let allProgressionArrUser = []
-    let allProgressionArrAuthor = []
-    let emptyElementProgression = '..'
-    let strElementProgration = ''
+    let startNum = Math.round(Math.random() * 10) + 1
+    let stepNum = Math.round(Math.random() * 10) + 1
+    let lengthNum = Math.round(Math.random() * 10) + 10
+    let emptyNum = Math.round(Math.random() * 10)
+    let resultArrAuthor = []
 
-    for (let i = 0; i < lengthProgression; i++) {
-      missingElementProgression === i ? (elementProgressionUser = emptyElementProgression) : (elementProgressionUser = startElementProgression + i * stepProgression)
-      elementProgressionAuthor = startElementProgression + i * stepProgression
-      allProgressionArrUser.push(elementProgressionUser)
-      allProgressionArrAuthor.push(elementProgressionAuthor)
-    }
-
-    strElementProgration = allProgressionArrUser.join(', ')
-    console.log(`Question: ${strElementProgration}`)
+    console.log(`Question: ${progressionCode(startNum, stepNum, lengthNum, emptyNum, resultArrAuthor)}`)
     const answer = Number.parseInt(readlineSync.question('Your answer: '))
 
-    if (answer === allProgressionArrAuthor[missingElementProgression]) {
+    if (answer === resultArrAuthor[0]) {
       console.log('Correct!')
     }
     else {
-      console.log(`${answer} is wrong answer ;(. Correct answer was ${allProgressionArrAuthor[missingElementProgression]}.\nLet's try again, ${nameUser}!`)
+      console.log(`${answer} is wrong answer ;(. Correct answer was ${resultArrAuthor[0]}.\nLet's try again, ${nameUser}!`)
       break
     }
 
