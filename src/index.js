8af495952e5cdf71f2f1dfa92b7529f9268f821a
Edik Kurbatov <iamedikkurbat0v@gmail.com>
2026-02-21 17:12:37 +0300

    replace logic code

--- src/index.js
+++ src/index.js
@@ -16,63 +16,14 @@ export default function makeRound(generationRound, descriptionTextGame) {
 
     const userAnswer = readlineSync.question('Your answer: ')
 
-    console.log(correctAnswer, userAnswer)
-    console.log(typeof(correctAnswer), typeof(userAnswer))
     if(correctAnswer === userAnswer) {
       console.log('Correct!')
     }
     else {
-      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.\nLet's try again, ${userName}!`)
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`)
 
       return
     }
   }
   console.log(`Congratulations, ${userName}!`)
 }
-
-// export function isPrimeNumber(num) {
-//   if (num < 2) return false
-//   if (num === 2) return true
-//   if (num % 2 === 0) return false
-
-//   for (let i = 3; i <= Math.sqrt(num); i += 2) {
-//     if (num % i === 0) {
-//       return false
-//     }
-//   }
-//   return true
-// }
-
-// export function findNod(a, b) {
-//   let bigNum = Math.max(a, b)
-//   let smallNum = Math.min(a, b)
-
-//   while (smallNum !== 0) {
-//     if (bigNum % smallNum === 0) {
-//       return smallNum
-//     }
-//     else {
-//       let temp = smallNum
-//       smallNum = bigNum % smallNum
-//       bigNum = temp
-//     }
-//   }
-//   return bigNum
-// }
-
-// export function progressionCode(start, step, length, empty, array) {
-//   let resultArrUser = []
-
-//   const lengthProgression = Math.min(length, 10)
-
-//   for (let i = 0; i < lengthProgression; i++) {
-//     if (i === empty) {
-//       resultArrUser.push('..')
-//       array.push(start + i * step)
-//     }
-//     else {
-//       resultArrUser.push(start + i * step)
-//     }
-//   }
-//   return resultArrUser.join(' ')
-// }
